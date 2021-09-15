85516a7bc2ba5891f146032a81bc12864fe385da
Andrew Hansbury <dru@Andrews-MacBook-Pro.local>
2021-09-15 04:30:11 -0400

    finished HW02

--- Web-Dev/212/HW02/index.php
+++ Web-Dev/212/HW02/index.php
@@ -1,7 +1,27 @@
 <!DOCTYPE hmtl>
 <html>
     <title>HW02</title>
+
+    <head>
+    </head>
     <h1>HW02 - Multiplication Table</h1>
+
+    
+
+    <style>
+    table, td {
+    border:1px solid black;
+    background-color: lightblue;
+    }
+    th {
+    
+    font-weight: bold;
+    border:1px solid black;
+    }
+    </style>
+
+    
+
 
     <?php 
     function convertHex($number){
@@ -23,8 +43,44 @@
 
     <body>
         <?php 
+
+
+        echo "<table> <th> Multiply <br> ";
         
-        echo "<tr> <th> Multiply </th> </tr>";
+        
+        for ($i = 1; $i<21; $i++){
+            echo '<td>' . $i . '</td>';
+        }
+        echo ' <tr>';
+        echo '<td> </td>';
+
+        for ($i = 1; $i<21; $i++){
+            echo '<td>' . convertHex($i) . '</td>';
+        }
+        
+        echo '</tr> </th>';
+
+        
+        for ($i = 0; $i<21; $i++){
+            echo '<tr>';
+            
+            if ($i != 0){
+                echo '<th>' . $i . '</th>';
+                echo '<th>' . convertHex($i) . '</th>';
+            }
+            
+            for ($j = 0; $j <21; $j++){
+                $num = convertHex($i*$j);
+                
+                echo '<td>' . $num . '</td>';
+            
+            }
+            echo '</tr>';
+        }
+
+        echo '</table>';
+
+
         ?>
     </body>
 
