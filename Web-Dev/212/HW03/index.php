c16162d7f9a00952ae02b61a99466dfb07108ee6
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-09-24 03:07:43 -0400

    fixed hw03 errors

--- Web-Dev/212/HW03/index.php
+++ Web-Dev/212/HW03/index.php
@@ -1,87 +1,139 @@
 <!DOCTYPE hmtl>
 <html>
-<title>HW03</title>
+<<<<<<< HEAD=======<title>HW03</title>
+    >>>>>>> 61ddad02c2f21c4b777c73644a210141b4cd43f0
 
-<head>
-</head>
-<h1>HW03 - Multiplication Table</h1>
-
-
-
-<style>
-    table,
-    td {
-        border: 1px solid black;
-        background-color: lightblue;
-    }
-
-    th {
-
-        font-weight: bold;
-        border: 1px solid black;
-    }
-</style>
+    <head>
+        <title>HW03</title>
+    </head>
 
 
-
-
-<?php
-function convertHex($number, $base)
-{
-
-    $converted = '';
-    while ($number > 0) {
-        $digit = (int) $number % $base;
-        if ($digit < 10) {
-            $converted = strval($digit) . "" . $converted;
-        } else {
-            $converted = chr($digit + 55) . "" . $converted;
+    <style>
+        table,
+        td {
+            border: 1px solid black;
+            background-color: lightblue;
         }
-        $number =  intdiv((int)$number, $base);
-    }
-    return $converted;
-}
-?>
 
-<body>
+        th {
+
+            font-weight: bold;
+            border: 1px solid black;
+        }
+    </style>
+
+
+
     <?php
+    function convertHex($number, $base)
+    {
+
+        $converted = '';
+        while ($number > 0) {
+            $digit = (int) $number % $base;
+            if ($digit < 10) {
+                $converted = strval($digit) . "" . $converted;
+            } else {
+                $converted = chr($digit + 55) . "" . $converted;
+            }
+            $number =  intdiv((int)$number, $base);
+        }
+        return $converted;
+    }
 
 
-    echo "<table>
+    function printTable()
+    {
+        echo "<table>
         <tr>
          <th rowspan = 2, colspan =2>Multiply </th> ";
 
-    for ($i = 1; $i < 21; $i++) {
-        echo '<th>' . $i . '</th>';
-    }
-    echo '</tr>';
-
-    echo '<tr>';
-    for ($i = 1; $i < 21; $i++) {
-        echo '<th>' . convertHex($i, 16) . '</th>';
-    }
-
-    echo '</tr> ';
-
-
-    for ($i = 1; $i < 21; $i++) {
-        echo '<tr>';
-        echo '<th>' . $i . '</th>';
-        echo '<th>' . convertHex($i, 16) . '</th>';
-
-
-        for ($j = 1; $j < 21; $j++) {
-            $num = convertHex($i * $j, 16);
-
-            echo '<td>' . $num . '</td>';
+        for ($i = 1; $i < 21; $i++) {
+            echo '<th>' . $i . '</th>';
         }
         echo '</tr>';
+
+        echo '<tr>';
+        for ($i = 1; $i < 21; $i++) {
+            echo '<th>' . convertHex($i, 16) . '</th>';
+        }
+
+        echo '</tr> ';
+
+        for ($i = 1; $i < 21; $i++) {
+            echo '<tr>';
+            echo '<th>' . $i . '</th>';
+            echo '<th>' . convertHex($i, 16) . '</th>';
+
+
+            for ($j = 1; $j < 21; $j++) {
+                $num = convertHex($i * $j, 16);
+
+                echo '<td>' . $num . '</td>';
+            }
+            echo '</tr>';
+        }
+
+        echo '</table>';
     }
 
-    echo '</table>';
-
-
     ?>
-</body>
+
+
+    <body>
+        <h1>HW03 - Multiplication Table</h1>
+
+
+        <?php
+        print_r($_POST);
+
+        if ($_POST) {
+            $fcolor = $_POST['color'];
+        } else {
+            $fcolor = "lightblue";
+        }
+        ?>
+
+        <form method="POST" action="">
+            <label for=colors> Select your background: Background: </label>
+            <select name="color">
+                <option value=White> White </option>
+                <option value=Lightblue> Lightblue </option>
+                <option value=Yellow> Yellow </option>
+                <option value=Coral> Coral </option>
+                <option value=Lightgreen> Lightgreen </option>
+            </select>
+        </form>
+        <br>
+
+        <p>Select the number base
+
+
+            <?php
+            //All the buttons
+            for ($i = 2; $i < 17; $i++) {
+                echo "
+        <input type=submit value = $i name = strval($i)>
+        ";
+            }
+            echo "</p>";
+
+            ?>
+
+
+
+
+
+
+
+            <?php
+
+            //Printing the multiplication table
+            printTable();
+
+
+            ?>
+
+    </body>
 
 </html>
