c03f59270363a4f2593e6063cec94b656c7faa43
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-09-27 01:19:06 -0400

    fixed part 1 of HW05

--- web-dev/212/HW05/index.php
+++ web-dev/212/HW05/index.php
@@ -3,6 +3,25 @@
 <html>
 <title>HW05</title>
 
+
+<?php
+$dir = "/home/andrewhansbury/public_html/CPTR-212/HW05//verbiage/";
+$files = [];
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            array_push($files, $file);
+        }
+        closedir($dh);
+    }
+}
+
+print_r($files);
+
+?>
+
+
 <body>
 
 
