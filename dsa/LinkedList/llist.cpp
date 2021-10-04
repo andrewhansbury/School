c1bbb4d9b567816e6ac5ba18e241558e0a845262
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-10-03 22:14:36 -0400

    lolz

--- dsa/LinkedList/llist.cpp
+++ dsa/LinkedList/llist.cpp
@@ -1,26 +1,105 @@
-//  Name: Andrew Hansbury
-//  Assignment number: 2 (?)
-//  Assignment: Linked Lists
-//  File name: llist.cpp
-//  Date last modified: September 27, 2021
-//  Honor statement: I have neither given nor received any unauthorized help on this assignment.
-#include <iostream>
-#include <string>
+//  name: annalisse chang, rebecca zaldivar, andrei modiga
+//  assignment number: 2
+//  assignment: c++ linked list
+//  file name: llist.cpp
+//  date last modified: oct 4, 2021
+//  honor statement: i have neither given nor received any unauthorized help on this assignment
+
 #include "llist.h"
+#include <string>
 
-LinkedList::Node::Node(const string &item) : data{item} {};
+LinkedList::LinkedList() : head(), tail(), len(0){};
 
-LinkedList::LinkedList() : head{&LinkedList::Node("*HEAD*")}, tail{&LinkedList::Node("*TAIL*")}
+LinkedList::Node::Node(const string &item) : data(item), next(new Node("")), prev(new Node("")) {}
+
+LinkedList::Iterator::Iterator(Node *p) : ptr(p) {}
+
+string &LinkedList::Iterator::operator*()
 {
-    head->next = tail;
-    tail->prev = head;
-    head->prev = nullptr;
-    tail->next = nullptr;
-};
 
-int main()
-{
-    LinkedList list1;
-    //std::cout << list1.head->data;
-    std::cout << "hi";
+    return this->ptr->data;
 }
+
+LinkedList::Iterator &LinkedList::Iterator::operator++()
+{
+    ptr = ptr->next;
+    return *this;
+}
+
+LinkedList::Iterator LinkedList::Iterator::operator++(int)
+{
+
+    Iterator Iter1 = *this;
+    ++(*this);
+    return Iter1;
+}
+
+LinkedList::Iterator &LinkedList::Iterator::operator--()
+{
+    ptr = ptr->prev;
+    return *this;
+}
+
+LinkedList::Iterator LinkedList::Iterator::operator--(int)
+{
+    Iterator Iter1 = *this;
+    --(*this);
+    return Iter1;
+}
+
+bool LinkedList::Iterator::operator==(const Iterator &other)
+{
+    return other.ptr == this->ptr;
+}
+
+bool LinkedList::Iterator::operator!=(const Iterator &other)
+{
+    return other.ptr != this->ptr;
+}
+
+LinkedList::Iterator LinkedList::begin() const
+{
+
+    return Iterator(head->next);
+}
+
+LinkedList::Iterator LinkedList::end() const
+{
+
+    return Iterator(tail);
+}
+
+int LinkedList::length() const
+{
+    return len;
+}
+
+void LinkedList::insert(const Iterator &iter, const string &item)
+{
+    Node *new_node = new Node(item);
+    new_node->next = iter.ptr;
+    new_node->prev = iter.ptr->prev;
+    iter.ptr->prev = new_node;
+    iter.ptr->prev->next = new_node;
+    len++;
+}
+
+LinkedList::LinkedList(const LinkedList &other) : head(new Node("")), tail(new Node("")), len(0)
+{
+    Iterator iter1 = other.begin();
+
+    while (iter1 != other.end())
+    {
+        insert(this->end(), iter1.ptr->data);
+        iter1++;
+    }
+}
+
+//deallocation funtion
+
+//assignment operator (iterator func)
+//remove
+//find (iterator func)
+//clear func
+//operator==
+//operator!=
---
+++ dsa/LinkedList/llist2.cpp
@@ -0,0 +1,31 @@
+//  Name: Andrew Hansbury
+//  Assignment number: 2 (?)
+//  Assignment: Linked Lists
+//  File name: llist.cpp
+//  Date last modified: September 27, 2021
+//  Honor statement: I have neither given nor received any unauthorized help on this assignment.
+#include <iostream>
+#include <string>
+#include "llist.h"
+
+LinkedList::Node::Node(const string &item) : data{item} {};
+
+LinkedList::LinkedList() : head{&LinkedList::Node("*HEAD*")}, tail{&LinkedList::Node("*TAIL*")}
+{
+    head->next = tail;
+    tail->prev = head;
+    head->prev = nullptr;
+    tail->next = nullptr;
+};
+
+LinkedList::LinkedList::getHead()
+{
+    return this->head;
+}
+
+int main()
+{
+    LinkedList list1;
+    //std::cout << list1.head->data;
+    std::cout << "hi";
+}
