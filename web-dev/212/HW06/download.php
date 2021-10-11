acf17a8108e63c7cb72ab13be6f7769f2f83c771
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-10-11 03:53:45 -0400

    finished hw06

---
+++ web-dev/212/HW06/download.php
@@ -0,0 +1,7 @@
+<?php
+
+$file = "results.txt";
+if (!file_exists($file)) die("File not found");
+header("Content-Disposition: attachment; filename=\"" . basename($file) . "\"");
+header("Content-Length: " . filesize($file));
+readfile($file);
--- web-dev/212/HW06/index.php
+++ web-dev/212/HW06/index.php
@@ -1,29 +1,43 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>HW06</title>
-    </head>
+
+<head>
+    <title>HW06</title>
+</head>
+
 <body>
 
-<h1>HW06 - Telephone Exchanges</h1>
+    <h1>HW06 - Telephone Exchanges</h1>
 
-<form action="index.php" method="post">
-City: <input type="text" name="city">
-State: <input type="text" name="state"><br>
-<input type="submit">
-</form>
+    <form action="index.php" method="post">
+        City: <input type="text" name="city" , value="<?php echo isset($_POST["city"]) ? $_POST["city"] : '' ?>">
+        State: <input type="text" name="state" , value="<?php echo isset($_POST["state"]) ? $_POST["state"] : '' ?>"><br>
+        <input type="submit">
+    </form>
 
-<?php
+    <?php
+    $book = file('allutlzd.txt');
 
-if (isset($_POST["city"])){
+    if (isset($_POST["city"]) && !empty($_POST['city'])) {
+        $city = strtoupper($_POST["city"]);
+        $state = strtoupper($_POST["state"]);
+        $full_list = "";
+        foreach ($book as $line) {
+            if (strpos($line, $city) !== false && strpos($line, $state) !== false) {
+                $result = substr($line, 3, 4) . ":" . substr($line, 8, 3) . ":" . $state . ":" . $city;
+                echo $result . "<br>";
+                $full_list .= $result . "\n";
+            }
+        }
 
-    echo "City: " . $_POST["city"] ;
-    echo "State: " . $_POST["state"];     
-
-}
-?>
-
+        $write_file = fopen("results.txt", "w");
+        fwrite($write_file, $full_list);
+        fclose($write_file);
+        echo " <form method=get action=download.php> <button type=submit>Download</button> </form>";
+    }
+    ?>
 
 
 </body>
+
 </html>
