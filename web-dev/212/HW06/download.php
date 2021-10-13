3b0b0cd6afc7285862a1e4a798cd1c7036572078
Andrew Hansbury <dru@Andrews-MacBook-Pro.local>
2021-10-13 09:50:30 -0400

    idek

--- web-dev/212/HW06/download.php
+++ web-dev/212/HW06/download.php
@@ -5,3 +5,5 @@ if (!file_exists($file)) die("File not found");
 header("Content-Disposition: attachment; filename=\"" . basename($file) . "\"");
 header("Content-Length: " . filesize($file));
 readfile($file);
+
+?>
