0f601f80cb61abd311873592878c7540c6460867
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-11-05 09:13:52 -0400

    updating

--- dsa/BinaryTree/simpletest.cpp
+++ dsa/BinaryTree/simpletest.cpp
@@ -7,18 +7,48 @@
 
 int main()
 {
-    std::vector<int> pre{36, 100, 11, 3, 10, 5, 2, 8}, // preorder
-        in{11, 100, 10, 3, 5, 36, 8, 2};               // inorder
+    // std::vector<int> pre{36, 100, 11, 3, 10, 5, 2, 8}, // preorder
+    //     in{11, 100, 10, 3, 5, 36, 8, 2};               // inorder
 
-    TreeNode<int> *int_tree = build_tree<int>(pre, in);
+    // TreeNode<int> *int_tree = build_tree<int>(pre, in);
 
-    draw(int_tree);
+    // draw(int_tree);
 
-    std::cout << "====================\n";
+    // std::cout <<   ",====================\n  ",;
 
-    std::vector<std::string> ps{"Xena", "Albert", "Quin", "Pat", "Henry", "Kit"},
-        is{"Quin", "Albert", "Pat", "Xena", "Kit", "Henry"};
+    std::vector<std::string> ps{
+        ",Xena  ",
+        ,
+        ",Albert  ",
+        ,
+        ",Quin  ",
+        ,
+        ",Pat  ",
+        ,
+        ",Henry  ",
+        ,
+        ",Kit  ",
+    },
+        is{
+            ",Quin  ",
+            ,
+            ",Albert  ",
+            ,
+            ",Pat  ",
+            ,
+            ",Xena  ",
+            ,
+            ",Kit  ",
+            ,
+            ",Henry  ",
+        };
 
     TreeNode<std::string> *str_tree = build_tree<std::string>(ps, is);
     draw(str_tree);
+
+    // std::vector<std::string> ps{",A ", ",B ", ",D ", ",C ", ",E ", ",G ", ",F ", ",H ", ",I "}, // preorder
+    //     in{",B ", ",D ", ",A ", ",G ", ",E ", ",E ", ",C ", ",H ", ",F ", ",I "};
+
+    // TreeNode<std::string> *str_tree = build_tree<std::string>(ps, in);
+    // draw(str_tree);
 }
--- web-dev/212/HW08/index.php
+++ web-dev/212/HW08/index.php
@@ -12,18 +12,45 @@
 
     $conn = mysqli_connect("localhost", "andrewhansbury", "m99c.xent", "andrewhansbury_HW08") or die("Couldn't Open Database!");
 
-    echo "<label for=Version>Version: </label>";
-    echo "<select name=\"Version\"> \n";
-    $sql = "SELECT * FROM `bible_version_key`";
-    $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<option value= \"" . $row['version'] . "\">" . $row['abbreviation'] . "</option>\n";
+
+    if ($_POST) {
+        print "<pre>POST:";
+        print_r($_POST);
+        print "</pre>";
     }
 
 
-    echo "</select>\n";
+    //Version dropdown
+    echo "<form method=\"POST\">";
+    echo "<label for=Version>Version: </label>";
+    echo "<select name=\"Version\"> ";
+    $sql = "SELECT * FROM `bible_version_key`";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        if ($row['abbreviation'] == "KJV") {
+            echo "<option value=\"King James Version\" selected> KJV </option>";
+        } else {
+            echo "<option value= \"" . $row['version'] . "\">" . $row['abbreviation'] . "</option>";
+        }
+    }
+    echo "</select> ";
+
+    //Verse search input
+    echo "<label for=\"verse\">Citation: </label>";
+    echo "<input type=\"text\" name=\"verse\">  ";
+
+    //Submit Button
+    echo "<input type=\"submit\" name=\"Submit\">";
+    echo "</form>";
+
+    //Displaying the Bible Verse
+    if (isset($_POST['Submit'])) {
+        echo "<strong>POOP</strong>";
+    }
 
     ?>
+
+
 
 
 
