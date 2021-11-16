fbdbeab4c4ff65150067217d3883434b70db5084
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-11-15 20:06:43 -0500

    small changes

--- dsa/Sorting/stuff.cpp
+++ dsa/Sorting/stuff.cpp
@@ -3,10 +3,12 @@
 #include <stopwatch.h>
 
 template <typename T>
-void selection_sort(std::vector<T>& v) {
+void selection_sort(std::vector<T> &v)
+{
     int n = v.size();
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
+    {
 
         int small = i;
 
@@ -15,37 +17,57 @@ void selection_sort(std::vector<T>& v) {
                 small = j; // Found a smaller value // Swap elements at position iter and small, if a smaller value was found if (i != small)
         if (i != small)
             std::swap(v[i], v[small]);
-        break;            
+        break;
     }
 }
 
 template <typename T>
-void exhange_sort(std::vector<T>& vec){
+void exhange_sort(std::vector<T> &vec)
+{
     int n = vec.size();
     int k = 0;
     bool sorted = false;
-    while(k < n -1 && !sorted){
+    while (k < n - 1 && !sorted)
+    {
         sorted = true;
-        for (int i=0; i<n-k-1; i++){
-            if (vec[i] > vec[i + 1]) {
-                std::swap(vec[i], vec[i+1]);
+        for (int i = 0; i < n - k - 1; i++)
+        {
+            if (vec[i] > vec[i + 1])
+            {
+                std::swap(vec[i], vec[i + 1]);
                 sorted = false;
             }
         }
         break;
         k++;
     }
-
 }
 
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
 
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
-int main(){
-    std::vector<int> vec = {7,2,3,1,5,4};
-    //selection_sort(vec);
+int main()
+{
+    std::vector<int> vec = {7, 2, 3, 1, 5, 4};
+    // selection_sort(vec);
 
     exhange_sort(vec);
-    for (int i = 0; i<vec.size(); i++){
-        std::cout<<vec[i];
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i];
     }
 }
