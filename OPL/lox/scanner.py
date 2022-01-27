832d7cd16d2ac33e79221d5e833f11462a7dcae1
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2022-01-27 01:44:42 -0500

    finished up to 4.5.1

--- OPL/lox/scanner.py
+++ OPL/lox/scanner.py
@@ -1,12 +1,57 @@
 
+from lox.tokentypes import TokenType
 from tokens import Token
 
 #may need to create the ENUMS (tokentype file)
 
 class Scanner:
     def __init__(self, source):
-        self.source = source
+        self.source:str = source
+        self.tokens = [Token]
+        
+        self.start = 0
+        self.current = 0
+        self.line = 1
         
 
-    def scanTokens() -> list[Token]:
-        pass 
+    def scanTokens(self) -> list[Token]:
+        while not self.isAtEnd():
+            self.start = self.current
+            self.scanToken()
+        
+        self.tokens.append(Token(Token.EOF,"", None, self.line))
+        return self.tokens
+    
+    def isAtEnd(self):
+        return self.current >= len(self.source)
+    
+    def scanToken(self):
+        c = self.advance()
+        match c:
+            case '(': self.addToken(Token.LEFT_PAREN)
+            case ')': self.addToken(Token.RIGHT_PAREN)
+            case '{': self.addToken(Token.LEFT_BRACE)
+            case '}': self.addToken(Token.RIGHT_BRACE)
+            case ',': self.addToken(Token.COMMA)
+            case '.': self.addToken(Token.DOT)
+            case '-': self.addToken(Token.MINUS)
+            case '+': self.addToken(Token.PLUS)            
+            case ';': self.addToken(Token.SEMICOLON) 
+            case '*': self.addToken(Token.STAR)
+            
+            
+    def advance(self):
+        self.current +=1
+        return self.source[self.current]
+
+    def addToken(self, type:TokenType):
+        self.addToken(type, None)
+        
+    def addToken(self, type:TokenType, literal:object ):
+        
+        text:str = self.source[self.start, self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
+        
+    
+        
+            
--- OPL/lox/tokentypes.py
+++ OPL/lox/tokentypes.py
@@ -1,3 +1,18 @@
-class TokenType:
-    def __init__(self):
-        pass
+from enum import Enum
+
+
+class TokenType(Enum):
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE = None
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR = None
+
+#   // One or two character tokens.
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL = None
+
+#   // Literals.
+  IDENTIFIER, STRING, NUMBER = None
+
+#   // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR = None
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE = None
+
+  EOF = None
